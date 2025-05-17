74e4db94ce172b369a0d241f0cb4c6ef18d294d8
WooKyoung Noh <wookay.noh@gmail.com>
2025-05-17 22:17:07 +0900

    test fails

---
+++ test/fails/test2.jl
@@ -0,0 +1,7 @@
+using Test
+
+@test true
+@test 3 == 1 + 2
+@test 5 == 1
+@test false
+@test false
---
+++ test/fails/test3.jl
@@ -0,0 +1,13 @@
+using Test
+
+@testset "error1" begin
+throw_error1
+end
+
+@testset "error2" begin
+throw_error2
+end
+
+@testset "error3" begin
+throw_error3
+end
