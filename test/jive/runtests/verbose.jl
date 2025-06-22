f29f8498a7b4965bdff794eb41bf5db1803fc7a1
WooKyoung Noh <wookay.noh@gmail.com>
2025-06-22 21:40:09 +0900

    runtests/verbose.jl enable_distributed=false

--- test/jive/runtests/verbose.jl
+++ test/jive/runtests/verbose.jl
@@ -4,10 +4,10 @@ using Test
 using Jive
 
 test_path = normpath(@__DIR__)
-output = @sprint_plain(runtests(test_path, targets="target3", verbose=true))
+output = @sprint_plain(runtests(test_path, targets="target3", verbose=true, enable_distributed=false))
 @test !isempty(output)
 
-output = @sprint_plain(runtests(test_path, targets="target3", verbose=false))
+output = @sprint_plain(runtests(test_path, targets="target3", verbose=false, enable_distributed=false))
 @test isempty(output)
 
 end # module test_jive_runtests_verbose
