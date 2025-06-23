9ab41b9a8d307df2e3dd8d9d8ad663c4508e14c9
WooKyoung Noh <wookay.noh@gmail.com>
2025-06-24 08:59:39 +0900

    run `JET.@report_call` in nightly

---
+++ test/scripts/jet.jl
@@ -0,0 +1,16 @@
+using JET: JET
+using .JET: ReportConfig, @report_call
+
+#=
+using .JET: watch_file
+using Revise
+using Jive
+jive_src_jivejl = String(Base.moduleloc(Jive).file)
+watch_file(jive_src_jivejl, report_config=report_config)
+=#
+
+using Jive
+using Distributed
+report_config = ReportConfig(#=target_modules=#(Jive,), #=ignored_modules=#(Distributed,))
+report = @report_call report_config=report_config runtests(normpath(@__DIR__, ".."); targets="testjive jive")
+show(report)
