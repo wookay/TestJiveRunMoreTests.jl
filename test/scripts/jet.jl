226c95c13c6b328a538ae2b6e641e9698e3040a1
WooKyoung Noh <wookay.noh@gmail.com>
2025-06-26 07:18:13 +0900

    print info during run jet.jl

--- test/scripts/jet.jl
+++ test/scripts/jet.jl
@@ -22,8 +22,9 @@ watch_file(jive_src_jivejl, report_config=report_config)
 
 using Jive
 using Distributed
+printstyled("JET.@report_call runtests\n", color = :cyan)
 report_config = ReportConfig(#=target_modules=#(Jive,), #=ignored_modules=#(Distributed,))
-report = @report_call report_config=report_config runtests(normpath(@__DIR__, ".."); targets="testjive jive")
+@time report = @report_call report_config=report_config runtests(normpath(@__DIR__, ".."); targets="testjive jive")
 show(report)
 if string(report) != "No errors detected\n"
     throw(ErrorException("`JET.@report_call runtests` found errors"))
