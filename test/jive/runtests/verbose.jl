86ad845c89ec1a5456b1d44840011cc9f21180e3
WooKyoung Noh <wookay.noh@gmail.com>
2026-07-05 16:10:38 +0900

    fix test run with `JULIA_TEST_VERBOSE=true`

--- test/jive/runtests/verbose.jl
+++ test/jive/runtests/verbose.jl
@@ -4,10 +4,19 @@ using Test
 using Jive
 
 test_path = normpath(@__DIR__)
-output = @sprint_plain(runtests(test_path, targets="target3", verbose=true, enable_distributed=false))
-@test !isempty(output)
 
 output = @sprint_plain(runtests(test_path, targets="target3", verbose=false, enable_distributed=false))
-@test isempty(output)
+m = match(r"""
+Starting testset: hello
+""", output)
+if m !== nothing
+    @test VERSION >= v"1.14-DEV"
+end
+
+output = @sprint_plain(runtests(test_path, targets="target3", verbose=true, enable_distributed=false))
+m = match(r"""
+target3.jl
+""", output)
+@test m !== nothing
 
 end # module test_jive_runtests_verbose
